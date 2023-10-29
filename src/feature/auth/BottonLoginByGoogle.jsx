3b603786cfa0597bbef42c42afa86286b7a68d29
PeakpoonPK <peakpoonamoroll@gmail.com>
2023-10-30 01:17:21 +0700

    UI Login (non-creat error)

---
+++ src/feature/auth/BottonLoginByGoogle.jsx
@@ -0,0 +1,10 @@
+import google from '../../assets/google.png'
+
+export default function BottonLoginByGoogle() {
+    return (
+        <button className='flex items-center bg-MonoColor-50 rounded-2xl w-[280px] gap-10 drop-shadow-xl  active:text-MonoColor-400'>
+            <img src={google} alt='googleLogin' className='w-[36px]' />
+            <span className='text-[16px] font-normal'>Sign in with google</span>
+        </button>
+    )
+}
---
+++ src/feature/auth/ButtonInputForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ButtonInputForm() {
+    return (
+        <div className='bg-Primary-dark rounded-2xl text-MonoColor-50 py-1 px-4 text-[18px] font-normal drop-shadow-lg active:text-MonoColor-400'>Login</div>
+    )
+}
