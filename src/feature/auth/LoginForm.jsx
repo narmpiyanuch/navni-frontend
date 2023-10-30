688893d6caab655fb3c8a6972773c1f5756cd143
Supaporn Makesirinapapong <supaporn@nestifly.com>
2023-10-30 15:46:00 +0700

    adding feature/login

---
+++ src/feature/auth/LoginForm.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { useAuth } from "../hook/use-auth";
+import ButtonInputForm from "./ButtonInputForm";
+import InputLoginForm from "./InputLoginForm";
+import loginSchema from "../../validators/Auth-validator";
+
+export default function LoginForm() {
+  const validateLogin = (input) => {
+    const { error } = loginSchema.validate(input, { abortEarly: false });
+    console.dir(error);
+    if (error) {
+      const result = error.details.reduce((acc, el) => {
+        const { message, path } = el;
+        acc[path[0]] = message;
+        return acc;
+      }, {});
+      return result;
+    }
+  };
+
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState({});
+
+  const { login } = useAuth();
+
+  const handleChangeInput = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    const validationError = validateLogin(input);
+    if (validationError) {
+      return setError(validationError);
+    }
+    setError({});
+    login(input).catch((error) => {
+      console.log(error);
+    });
+  };
+
+  return (
+    <form
+      className="flex flex-col items-center gap-4 pt-8"
+      onSubmit={handleSubmitForm}
+    >
+      <div className="flex flex-col gap-4 items-end">
+        <InputLoginForm
+          title={"Email"}
+          value={input.email}
+          onChange={handleChangeInput}
+          hasError={error.email}
+        />
+        <InputLoginForm
+          title={"Password"}
+          value={input.password}
+          type="password"
+          onChange={handleChangeInput}
+          hasError={error.password}
+        />
+      </div>
+      <ButtonInputForm title="Log In" />
+    </form>
+  );
+}
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,30 +1,34 @@
-import { createBrowserRouter } from "react-router-dom"
-import LoginPage from "../pages/LoginPage"
-import { Outlet } from "react-router-dom"
-import HomePageForUser from "../pages/userPage/HomePageForUser"
-import RegisterPageForUser from "../pages/userPage/RegisterPageForUser"
-import { RouterProvider } from "react-router-dom"
+import { createBrowserRouter } from "react-router-dom";
+import LoginPage from "../pages/LoginPage";
+import { Outlet } from "react-router-dom";
+import HomePageForUser from "../pages/userPage/HomePageForUser";
+import RegisterPageForUser from "../pages/userPage/RegisterPageForUser";
+import { RouterProvider } from "react-router-dom";
+import RedirectIfAuthenticated from "../feature/auth/RedirectIfAuthenticated";
 
 const router = createBrowserRouter([
-    {
-        path: 'login',
-        element: <LoginPage />
-    },
-    {
-        path: '/',
-        element: (
-            <div>
-                <Outlet />
-            </div>
-        ), children: [
-            { path: '/', element: <HomePageForUser /> },
-            { path: '/register', element: <RegisterPageForUser /> }
-        ]
-    }
+  {
+    path: "login",
+    element: (
+      <RedirectIfAuthenticated>
+        <LoginPage />
+      </RedirectIfAuthenticated>
+    ),
+  },
+  {
+    path: "/",
+    element: (
+      <div>
+        <Outlet />
+      </div>
+    ),
+    children: [
+      { path: "/", element: <HomePageForUser /> },
+      { path: "/register", element: <RegisterPageForUser /> },
+    ],
+  },
 ]);
 
 export default function Router() {
-    return (
-        <RouterProvider router={router} />
-    )
+  return <RouterProvider router={router} />;
 }
