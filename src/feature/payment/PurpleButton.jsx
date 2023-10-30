ed07f99e46eaac4b96e7da38fe98039dc75553b3
PeakpoonPK <peakpoonamoroll@gmail.com>
2023-10-31 01:24:14 +0700

    finish for TaxiPage, both modal for choose pin

--- src/feature/payment/PurpleButton.jsx
+++ src/feature/payment/PurpleButton.jsx
@@ -2,6 +2,6 @@
 
 export default function PurpleButton({ title }) {
     return (
-        <div className='bg-Primary-dark rounded-2xl text-MonoColor-50 py-2 px-10 text-[18px] font-normal  active:text-MonoColor-600 active:bg-Primary-light'>{title}</div>
+        <button className='bg-Primary-dark rounded-2xl text-MonoColor-50 py-2 px-10 text-[18px] font-normal  active:text-MonoColor-600 active:bg-Primary-light'>{title}</button>
     )
 }
---
+++ src/feature/taxi/WhiteButton.jsx
@@ -0,0 +1,7 @@
+
+
+export default function WhiteButton({ title }) {
+    return (
+        <button button className='bg-MonoColor-50 rounded-2xl border-2 border-Primary-dark text-Primary-dark w-[160px] h-[36px] text-[14px] font-normal  active:text-MonoColor-600 active:bg-Primary-light'>{title}</button>
+    )
+}
