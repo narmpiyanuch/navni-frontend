e4e7fefe5c43efd9385cf98df34df1a1adf0e2d4
PeakpoonPK <peakpoonamoroll@gmail.com>
2023-11-01 11:54:27 +0700

    edit something about size in loginPage and registerdiverPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,7 +5,7 @@ import LoginForm from "../feature/auth/LoginForm";
 export default function LoginPage() {
   return (
     <div className="flex flex-col m-auto items-center justify-center bg-Primary-main h-screen w-screen gap-2">
-      <div className="flex flex-col fixed bottom-[40%] items-center ">
+      <div className="flex flex-col fixed bottom-[36%] items-center ">
         <div className=" relative flex flex-col items-center  ">
           <img src={whiteLogo} alt="whitLogo" className="w-[240px]" />
           <p className="text-MonoColor-50 font-bold text-[20px] absolute top-[220px]">
