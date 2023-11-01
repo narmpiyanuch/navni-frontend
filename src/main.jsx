1c5f6be5817487f9a7b496b0a72b5701143ef93b
money <supalap.ss@gmail.com>
2023-11-01 14:27:49 +0700

    commit before sync

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,11 @@ import AuthContextProvider from "./context/AuthContext.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <GoogleOAuthProvider clientId="1057347670375-8he167c3ublu3j6k4h9knm8qdn4f5r96.apps.googleusercontent.com">
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </GoogleOAuthProvider>
-  // </React.StrictMode>,
+    // <React.StrictMode>
+    <GoogleOAuthProvider clientId="1057347670375-8he167c3ublu3j6k4h9knm8qdn4f5r96.apps.googleusercontent.com">
+        <AuthContextProvider>
+            <App />
+        </AuthContextProvider>
+    </GoogleOAuthProvider>
+    // </React.StrictMode>,
 );
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 import { createBrowserRouter } from "react-router-dom";
-import LoginPage from "../pages/LoginPage";
 import { Outlet } from "react-router-dom";
+import LoginPage from "../pages/LoginPage";
 import HomePageForUser from "../pages/userPage/HomePageForUser";
 import RegisterPageForUser from "../pages/userPage/RegisterPageForUser";
 import { RouterProvider } from "react-router-dom";
@@ -10,94 +9,45 @@ import TopUpToMyWalletPage from "../pages/userPage/PaymentFlowPage/TopUpToMyWall
 import QRcodePaymentPage from "../pages/userPage/PaymentFlowPage/QRcodePaymentPage";
 import TopUpSuccess from "../pages/userPage/PaymentFlowPage/TopUpSuccess";
 import TopUpFail from "../pages/userPage/PaymentFlowPage/TopUpFail";
-import RedirectIfAuthenticated from "../feature/auth/RedirectIfAuthenticated";
-import RedirectIfNotAuthenticated from "../feature/auth/RedirectIfNotAuthenticated";
+import TaxiPage from "../pages/userPage/Booking/TaxiPage";
+import ChooseNemberPeoplePage from "../pages/userPage/Booking/ChooseNemberPeoplePage";
+import WaitingTaxiPage from "../pages/userPage/Booking/WaitingTaxiPage";
+import OnTheWayPage from "../pages/userPage/Booking/OnTheWayPage";
+import ServiceSuccesPage from "../pages/userPage/Booking/ServiceSuccesPage";
+import ServiceHistory from "../pages/userPage/ServiceHistory";
+import InformationPage from "../pages/userPage/InformationPage";
 
 const router = createBrowserRouter([
-  {
-    path: "login",
-    element: (
-      <RedirectIfAuthenticated>
-        <LoginPage />
-      </RedirectIfAuthenticated>
-    ),
-  },
-  {
-    path: "/register",
-    element: (
-      <RedirectIfAuthenticated>
-        <RegisterPageForUser />,
-      </RedirectIfAuthenticated>
-    ),
-  },
-  {
-    path: "/",
-    element: (
-      <RedirectIfNotAuthenticated>
-        <Outlet />
-      </RedirectIfNotAuthenticated>
-    ),
-    children: [
-      { path: "/", element: <HomePageForUser /> },
-
-      { path: "/topup", element: <TopUpToAmoutPage /> },
-      { path: "/topupmywallet", element: <TopUpToMyWalletPage /> },
-      { path: "/qrcode", element: <QRcodePaymentPage /> },
-      { path: "/successtopup", element: <TopUpSuccess /> },
-      { path: "/failtopup", element: <TopUpFail /> },
-    ],
-  },
-=======
-import { createBrowserRouter } from "react-router-dom"
-import LoginPage from "../pages/LoginPage"
-import { Outlet } from "react-router-dom"
-import HomePageForUser from "../pages/userPage/HomePageForUser"
-import RegisterPageForUser from "../pages/userPage/RegisterPageForUser"
-import { RouterProvider } from "react-router-dom"
-import TopUpToAmoutPage from "../pages/userPage/PaymentFlowPage/TopUpToAmoutPage"
-import TopUpToMyWalletPage from "../pages/userPage/PaymentFlowPage/TopUpToMyWalletPage"
-import QRcodePaymentPage from "../pages/userPage/PaymentFlowPage/QRcodePaymentPage"
-import TopUpSuccess from "../pages/userPage/PaymentFlowPage/TopUpSuccess"
-import TopUpFail from "../pages/userPage/PaymentFlowPage/TopUpFail"
-import TaxiPage from "../pages/userPage/Booking/TaxiPage"
-import ChooseNemberPeoplePage from "../pages/userPage/Booking/ChooseNemberPeoplePage"
-import WaitingTaxiPage from "../pages/userPage/Booking/WaitingTaxiPage"
-import OnTheWayPage from "../pages/userPage/Booking/OnTheWayPage"
-import ServiceSuccesPage from "../pages/userPage/Booking/ServiceSuccesPage"
-import ServiceHistory from "../pages/userPage/ServiceHistory"
-import InformationPage from "../pages/userPage/InformationPage"
-
-const router = createBrowserRouter([
-  {
-    path: 'login',
-    element: <LoginPage />
-  },
-  {
-    path: '/',
-    element: (
-      <div>
-        <Outlet />
-      </div>
-    ), children: [
-      { path: '/', element: <HomePageForUser /> },
-      { path: '/register', element: <RegisterPageForUser /> },
-      { path: '/topup', element: <TopUpToAmoutPage /> },
-      { path: '/topupmywallet', element: <TopUpToMyWalletPage /> },
-      { path: '/qrcode', element: <QRcodePaymentPage /> },
-      { path: '/successtopup', element: <TopUpSuccess /> },
-      { path: '/failtopup', element: <TopUpFail /> },
-      { path: '/taxi', element: <TaxiPage /> },
-      { path: '/numberpeople', element: <ChooseNemberPeoplePage /> },
-      { path: '/waitingtaxi', element: <WaitingTaxiPage /> },
-      { path: '/ontheway', element: <OnTheWayPage /> },
-      { path: '/serviceend', element: <ServiceSuccesPage /> },
-      { path: '/servicehistory', element: <ServiceHistory /> },
-      { path: '/information', element: <InformationPage /> },
-    ]
-  }
->>>>>>> develop
+    {
+        path: "login",
+        element: <LoginPage />,
+    },
+    {
+        path: "/",
+        element: (
+            <div>
+                <Outlet />
+            </div>
+        ),
+        children: [
+            { path: "/", element: <HomePageForUser /> },
+            { path: "/register", element: <RegisterPageForUser /> },
+            { path: "/topup", element: <TopUpToAmoutPage /> },
+            { path: "/topupmywallet", element: <TopUpToMyWalletPage /> },
+            { path: "/qrcode", element: <QRcodePaymentPage /> },
+            { path: "/successtopup", element: <TopUpSuccess /> },
+            { path: "/failtopup", element: <TopUpFail /> },
+            { path: "/taxi", element: <TaxiPage /> },
+            { path: "/numberpeople", element: <ChooseNemberPeoplePage /> },
+            { path: "/waitingtaxi", element: <WaitingTaxiPage /> },
+            { path: "/ontheway", element: <OnTheWayPage /> },
+            { path: "/serviceend", element: <ServiceSuccesPage /> },
+            { path: "/servicehistory", element: <ServiceHistory /> },
+            { path: "/information", element: <InformationPage /> },
+        ],
+    },
 ]);
 
 export default function Router() {
-  return <RouterProvider router={router} />;
+    return <RouterProvider router={router} />;
 }
