581b886108598eadaaea880bc6059f2e1917caeb
money <supalap.ss@gmail.com>
2023-11-02 15:23:32 +0700

    commit after sync

--- src/feature/auth/LoginForm.jsx
+++ src/feature/auth/LoginForm.jsx
@@ -10,32 +10,11 @@ import InputErrorMessage from "./InputErrorMessage";
 import validaterFn from "../../validators/validateFN";
 
 export default function LoginForm() {
-  const [input, setInput] = useState({
-    email: "",
-    password: "",
-  });
-
-  const { login } = useAuth();
-
-  const [error, setError] = useState({});
-
-  const onError = (res) => {
-    console.log("failed", res);
-  };
-
-  const handleSubmitForm = (e) => {
-    e.preventDefault();
-    const validationError = validaterFn(loginSchema, input);
-    if (validationError) {
-      return setError(validationError);
-    }
-    setError({});
-    login(input).catch((error) => {
-      console.log(error);
+    const [input, setInput] = useState({
+        email: "",
+        password: "",
     });
-  };
 
-<<<<<<< HEAD
     const { login } = useAuth();
 
     const [error, setError] = useState({});
@@ -70,7 +49,7 @@ export default function LoginForm() {
                     onChange={(e) =>
                         setInput({ ...input, email: e.target.value })
                     }
-                    // hasError={error.email}
+                    hasError={error.email}
                     type="email"
                 />
                 {error.email && <InputErrorMessage message={error.email} />}
@@ -81,7 +60,7 @@ export default function LoginForm() {
                     onChange={(e) =>
                         setInput({ ...input, password: e.target.value })
                     }
-                    // hasError={error.password}
+                    hasError={error.password}
                 />
                 {error.password && (
                     <InputErrorMessage message={error.password} />
@@ -99,43 +78,4 @@ export default function LoginForm() {
             />
         </form>
     );
-=======
-  return (
-    <form
-      className="flex flex-col items-center gap-4 pt-8"
-      onSubmit={(event) => {
-        handleSubmitForm(event);
-      }}
-    >
-      <div className="flex flex-col gap-4 items-end">
-        <InputLoginForm
-          title={"Email"}
-          value={input.email}
-          onChange={(e) => setInput({ ...input, email: e.target.value })}
-          hasError={error.email}
-          type="email"
-        />
-        {error.email && <InputErrorMessage message={error.email} />}
-        <InputLoginForm
-          title={"Password"}
-          value={input.password}
-          type="password"
-          onChange={(e) => setInput({ ...input, password: e.target.value })}
-          hasError={error.password}
-        />
-        {error.password && <InputErrorMessage message={error.password} />}
-      </div>
-      <ButtonInputForm title="Log In" type={"submit"} />
-      <p className="text-MonoColor-50 text-[12px] py-2">OR</p>
-      <GoogleLogin
-        onSuccess={(credentialResponse) => {
-          const userData = jwtDecode(credentialResponse.credential);
-          login(userData);
-          console.log(userData);
-        }}
-        onError={onError}
-      />
-    </form>
-  );
->>>>>>> 09e86ae55ecbc292d8aa9f4e8df43f4e3e1ef681
 }
--- src/utils/Loading.jsx
+++ src/utils/Loading.jsx
@@ -1,11 +1,11 @@
 export default function Loading({ isLoading }) {
-  return (
-    <div
-      className={`fixed top-0 left-0 w-full h-full flex items-center justify-center bg-white z-50 transition-opacity ${
-        isLoading ? "opacity-30" : "opacity-0"
-      }`}
-    >
-      <span className="loading loading-dots loading-lg"></span>;
-    </div>
-  );
+    return (
+        <div
+            className={`fixed top-0 left-0 w-full h-full flex items-center justify-center bg-white z-50 transition-opacity ${
+                isLoading ? "opacity-30" : "opacity-0"
+            }`}
+        >
+            <span className="loading loading-dots loading-lg bg-Primary-main"></span>
+        </div>
+    );
 }
