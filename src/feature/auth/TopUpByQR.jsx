632e4a2ac8cc949ca497da2d0e7003ea651b511b
narmpiyanuch <a.piyanuch@hotmail.com>
2023-11-03 11:07:52 +0700

    finish-full-stack-myHistoryWallet

--- src/feature/auth/TopUpByQR.jsx
+++ src/feature/auth/TopUpByQR.jsx
@@ -1,37 +1,41 @@
-const date_time_topup = [
-    { id: 1, amount: '-20 NP', balance: '100 NP', date_time: '25 Oct 13, 22:40', topup: 'Top-up by QR code' },
-    { id: 2, amount: '+100 NP', balance: '100 NP', date_time: '25 Oct 13, 08:40', topup: 'Top-up by QR code' },
-    { id: 3, amount: '-37 NP', balance: '0 NP', date_time: '25 Oct 13, 22:40', topup: 'Top-up by QR code' },
-]
+// const date_time_topup = [
+//     { id: 1, amount: '-20 NP', balance: '100 NP', date_time: '25 Oct 13, 22:40', topup: 'Top-up by QR code' },
+//     { id: 2, amount: '+100 NP', balance: '100 NP', date_time: '25 Oct 13, 08:40', topup: 'Top-up by QR code' },
+//     { id: 3, amount: '-37 NP', balance: '0 NP', date_time: '25 Oct 13, 22:40', topup: 'Top-up by QR code' },
+// ]
+import dayjs from "dayjs";
 
+export default function TopUpByQR({ history }) {
+  console.log(history?.allTransaction);
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-col pt-8 justify-center align-middle">
+        {history?.allTransaction.map((item) => {
+          return (
+            <>
+              <div className="flex gap-6 text-[14px] justify-between align-middle">
+                <div>
+                  <p className="text-MonoColor-400">
+                    {dayjs(item.createdAt).format("D MMM, YYYY h:mm A")}
+                  </p>
+                  <p className="px-6">
+                    {item.method ? `top-up by${item.method}` : "paid"}
+                  </p>
+                </div>
 
-export default function TopUpByQR() {
-    return (
-        <div className="flex flex-col">
-            <div className="flex flex-col pt-8 justify-center align-middle">
-                {date_time_topup.map(date_time_topup => {
-                    return (
-                        <>
-                            <div className="flex gap-6 text-[14px] justify-between align-middle">
-                                <div key={date_time_topup.id} >
-                                    <p className="text-MonoColor-400">{date_time_topup.date_time}</p>
-                                    <p className="px-6">{date_time_topup.topup}</p>
-                                </div>
-
-                                <div key={date_time_topup.id} className="text-[14px] py-5">
-                                    <p>Amount</p>
-                                    <p className="text-Error-pressed">{date_time_topup.amount}</p>
-                                </div>
-
-                                <div key={date_time_topup.id} className="text-[14px] py-5">
-                                    <p>Balance</p>
-                                    <p className="text-Primary-dark">{date_time_topup.balance}</p>
-                                </div>
-                            </div>
-                        </>
-                    )
-                })}
-            </div>
-        </div>
-    )
+                <div className="text-[14px] py-5">
+                  <p>Amount</p>
+                  {item.method ? (
+                    <p className="text-OtherColor-darkGreen">{item.price}</p>
+                  ) : (
+                    <p className="text-Error-pressed">{item.price}</p>
+                  )}
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
--- src/pages/userPage/MyWalletPage.jsx
+++ src/pages/userPage/MyWalletPage.jsx
@@ -1,33 +1,55 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import TopUpByQR from "../../feature/auth/TopUpByQR";
 import WalletButton from "../../feature/payment/WalletButton";
+import axios from "../../config/axios";
 
 export default function MyWalletPage() {
-    return (
-        <div className="flex flex-col items-center">
+  const [history, setHistory] = useState();
 
-            <div className="flex pt-10 items-start w-full">
-                <Link to='/'>
-                    <span className="material-symbols-outlined text-Primary-dark  font-bold text-[32px] active:text-MonoColor-300 pl-4"> navigate_before</span>
-                </Link>
-            </div>
+  const fetchHistory = async () => {
+    try {
+      const result = await axios.get("/user/wallet");
+      console.log("result", result.data);
+      setHistory(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-            <div className="">
-                <p className="text-Primary-dark text-[24px] font-normal item-center">My Wallet</p>
-            </div>
+  useEffect(() => {
+    fetchHistory();
+  }, []);
 
-            <div className="pt-8">
-                <p className="text-[18px] text-MonoColor-400">Current balance</p>
-            </div>
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex pt-10 items-start w-full">
+        <Link to="/home">
+          <span className="material-symbols-outlined text-Primary-dark  font-bold text-[32px] active:text-MonoColor-300 pl-4">
+            navigate_before
+          </span>
+        </Link>
+      </div>
 
-            <WalletButton title='100 NP +' />
+      <div className="">
+        <p className="text-Primary-dark text-[24px] font-normal item-center">
+          My Wallet
+        </p>
+      </div>
 
-            <div className="pt-10">
-                <p className="text-[18px] text-MonoColor-400">My Wallet History</p>
-            </div>
+      <div className="pt-8">
+        <p className="text-[18px] text-MonoColor-400">Current balance</p>
+      </div>
 
-            <TopUpByQR />
+      <Link to="/topupmywallet">
+        <WalletButton key={history?.id} title={`${history?.amount} NP +`} />
+      </Link>
 
-        </div>
-    )
+      <div className="pt-10">
+        <p className="text-[18px] text-MonoColor-400">My Wallet History</p>
+      </div>
+
+      <TopUpByQR key={history?.id} history={history} />
+    </div>
+  );
 }
