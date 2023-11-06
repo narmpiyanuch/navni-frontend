19ca3ae6f4cc98a93c6fe34b444d272383abf525
money <supalap.ss@gmail.com>
2023-11-06 15:47:42 +0700

    fixed bug in myWalletPage

--- src/feature/payment/WalletButton.jsx
+++ src/feature/payment/WalletButton.jsx
@@ -1,7 +1,7 @@
-export default function WalletButton({ title }) {
+export default function WalletButton({ children }) {
     return (
         <button className="bg-Primary-dark rounded-2xl text-MonoColor-100 py-3 px-10 text-[36px] font-normal text-center flex gap-4 justify-center items-center min-w-full">
-            {title}
+            {children}
             <div className="bg-MonoColor-100 w-9 h-9 rounded-full text-Primary-dark flex items-center justify-center">
                 +
             </div>
