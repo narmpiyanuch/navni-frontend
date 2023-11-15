f7cc6b6965a09d37fe5a04169907620a99dc9831
PeakpoonPK <peakpoonamoroll@gmail.com>
2023-11-15 11:02:00 +0700

    edit size Modal

--- src/feature/driver/HeaderDriver.jsx
+++ src/feature/driver/HeaderDriver.jsx
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 import useDriver from '../hook/use-driver';
 
 export default function HeaderDriver() {
-  const {profile}=useDriver()
+  const { profile } = useDriver()
   console.log(profile)
   return (
-    <div className="flex fixed top-0 flex-col items-center justify-start bg-Primary-main w-screen h-[180px] rounded-[0px_0px_40px_40px]">
-      <div className=" flex flex-col pt-10 items-start w-full ">
+    <div className="flex fixed top-0 flex-col items-center justify-start bg-Primary-main w-screen h-[160px] rounded-[0px_0px_40px_40px]">
+      <div className=" flex flex-col pt-4 items-start w-full ">
         <span className="material-symbols-outlined text-Primary-dark  font-bold text-[32px] active:text-MonoColor-300 pl-4">
           <Link to="/homedriver">navigate_before</Link>
         </span>
       </div>
-      <div className="flex fixed gap-4 top-[10%] items-center">
+      <div className="flex fixed gap-4 top-[6%] items-center">
         <img
           src={profile?.image ? profile?.image : user}
           className="rounded-full aspect-square w-[60px] border-4 border-MonoColor-50 bg-MonoColor-50 drop-shadow-lg"
@@ -27,7 +27,7 @@ export default function HeaderDriver() {
           <hr className="w-[240px] border-[1.5px] rounded-xl" />
           <div className="flex flex-col gap-2 justify-between item-center">
             <div className="flex gap-2 items-center">
-            <span className="material-symbols-outlined text-MonoColor-50 font-thin text-[18px]">
+              <span className="material-symbols-outlined text-MonoColor-50 font-thin text-[18px]">
                 directions_car
               </span>
               <p className="text-[14px] text-MonoColor-50"> {profile && profile?.plateNumber}</p>
--- src/feature/driver/OrderForDriver.jsx
+++ src/feature/driver/OrderForDriver.jsx
@@ -4,7 +4,7 @@ import OrderCustumerOne from "./OrderCustumerOne";
 export default function OrderForDriver({ setIsAccept, onClose }) {
   return (
     <>
-      <div className=" bg-MonoColor-50 pt-2 flex flex-col gap-2 fixed items-center justify-start bottom-0 w-full max-h-[40vh] overflow-auto">
+      <div className=" bg-MonoColor-50 pt-2 flex flex-col gap-2 fixed items-center justify-start bottom-0 w-full max-h-[32vh] drop-shadow-xl overflow-auto">
         <OrderCustumerOne />
         <OrderCustumerOne />
         <OrderCustumerOne />
@@ -13,8 +13,6 @@ export default function OrderForDriver({ setIsAccept, onClose }) {
         <OrderCustumerOne />
         <OrderCustumerOne />
         <OrderCustumerOne />
-
-
       </div>
     </>
   )
